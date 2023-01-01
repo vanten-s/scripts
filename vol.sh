8a04ad4d1340ae326ca5c87d5504850aba68ff74
vanten-s <svante.spannar@gmail.com>
2023-01-01 15:01:05 +0100

    Vol lmao

--- vol.sh
+++ vol.sh
@@ -15,19 +15,6 @@ fi
 
 VOLUME="$(pulsemixer --get-volume | sed '~s/. .//')"
 MUTE=$(echo $AMIXER | grep -o '\[off\]' | tail -n 1)
-if [ "$VOLUME" -le 20 ]; then
-    ICON=audio-volume-low
-else if [ "$VOLUME" -le 60 ]; then
-         ICON=audio-volume-medium
-     else
-         ICON=audio-volume-high
-     fi
-fi
-if [ "$MUTE" == "[off]" ]; then
-    ICON=audio-volume-muted
-fi
-
-echo $VOLUME
 
 if cat /tmp/addr.tmp; then
     notify-send "Headphones" "$VOLUME/100" -h int:value:$VOLUME -r "$(cat /tmp/addr.tmp)" -p > /tmp/addr.tmp
