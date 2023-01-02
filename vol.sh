966d15a28a7dfc3e658f207bfaffc883c6107f74
vanten-s <svante.spannar@gmail.com>
2023-01-02 20:38:21 +0100

    Fixed vol again

--- vol.sh
+++ vol.sh
@@ -15,10 +15,11 @@ fi
 
 VOLUME="$(pulsemixer --get-volume | sed '~s/. .//')"
 
-if [ -z cat /tmp/addr_vol ]; then
+touch /tmp/addr_vol
+if [ -z $(cat /tmp/addr_vol) ]; then
     notify-send "Headphones" "$VOLUME/100" -h int:value:$VOLUME -p > /tmp/addr_vol
     notify-send "Didn't find anything!" "Vol.sh didn't find /tmp/addr_vol"
 else
-    notify-send "Headphones" "$VOLUME/100" -h int:value:$VOLUME -r "$(cat /tmp/addr.tmp)" -p > /tmp/addr_vol
+    notify-send "Headphones" "$VOLUME/100" -h int:value:$VOLUME -r "$(cat /tmp/addr_vol)" -p > /tmp/addr_vol
 fi
 
